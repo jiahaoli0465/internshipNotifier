7c65b044a2758d3ac864f51a8d726c6f5affa2d6
Jiahao Li <jiahaoli0465@gmail.com>
2024-07-13 15:45:05 -0400

    set up

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world! to me</h1>;
 };
 
 export default Home;
