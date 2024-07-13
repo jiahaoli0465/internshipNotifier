2849e5d574ccb2a424e3865184ec3e5c83c96029
Jiahao Li <jiahaoli0465@gmail.com>
2024-07-13 15:40:55 -0400

    set up

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,30 @@
+// src/layouts/MainLayout.tsx
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const MainLayout: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default MainLayout;
